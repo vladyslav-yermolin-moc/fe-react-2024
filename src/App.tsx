0c3aa21257fc46245e8e00b3454237c7bd219732
Vladyslav Yermolin <vladyslav.yermolin@masterofcode.com>
2024-04-06 20:01:19 +0300

    Broken commit

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,14 @@ function App() {
     return (
         <>
             <HeaderComponent />
+            <dfdfdfffg
             <div className="container">
                 <p>
                     Hi! My name is [Name] and I&#39;m a Junior Frontend Developer. I am already familiar with main Web Technologies like
                     React, HTML, CSS, JavaScript and Git version control system.
                 </p>
                 <p>This page was developed during the course &#39;Intro to React&#39; from Masters Academy in 2024.</p>
-                <p>
-                    This is a social project from MOCG company where I got an opportunity to work with Frontend mentors and to create my own
-                    small project for the portfolio.
-                </p>
+                <p>This is a small project for the portfolio.</p>
                 <p>You can contact me via [social network name] and check out my GitHub.</p>
             </div>
         </>
