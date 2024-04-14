e28c1a7e524a5e40da17e3be86196c510f5ea9b4
Vladyslav Yermolin <vladyslav.yermolin@masterofcode.com>
2024-04-14 10:50:03 +0300

    Add image

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,20 @@ function App() {
     return (
         <>
             <HeaderComponent />
-            <dfdfdfffg
             <div className="container">
-                <p>
-                    Hi! My name is [Name] and I&#39;m a Junior Frontend Developer. I am already familiar with main Web Technologies like
-                    React, HTML, CSS, JavaScript and Git version control system.
-                </p>
-                <p>This page was developed during the course &#39;Intro to React&#39; from Masters Academy in 2024.</p>
-                <p>This is a small project for the portfolio.</p>
-                <p>You can contact me via [social network name] and check out my GitHub.</p>
+                <div className="left">
+                    <h1>About me</h1>
+                    <p>
+                        Hi! My name is [Name] and I&#39;m a Junior Frontend Developer. I am already familiar with main Web Technologies like
+                        React, HTML, CSS, JavaScript and Git version control system.
+                    </p>
+                    <p>This page was developed during the course &#39;Intro to React&#39; from Masters Academy in 2024.</p>
+                    <p>This is a small project for the portfolio.</p>
+                    <p>You can contact me via [social network name] and check out my GitHub.</p>
+                </div>
+                <div className="right">
+                    <img src="./octocat.png" alt="Octocat" className="about-img" />
+                </div>
             </div>
         </>
     );
